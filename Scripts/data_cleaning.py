a572c6c8141ca1692e0b557790fdecca4e6961c4
Erla Hrafnkelsdóttir <erla.hrafnkelsdottir@gmail.com>
2022-02-17 23:05:13 +0100

    End of day push

--- Scripts/data_cleaning.py
+++ Scripts/data_cleaning.py
@@ -7,29 +7,26 @@ warnings.filterwarnings("ignore")
 
 
 # Global variables
-# The T3 columns are sometimes wrongly labelled and Cz is sometimes zero
-# Therefore, we omit these columns from our clean data and remove them from the list below
+# OBS: The T3 electrode is sometimes referred to A2 instead of A1
+# We don't let it bother us for now as it likely doesn't have much effect on calculations
 # fmt: off
 electrodes_arranged = [
     'Fp1-A1', 'Fpz-A1', 'Fp2-A2', 'F7-A1', 'F3-A1', 'Fz-A1', 'F4-A2', 'F8-A2', 'FT7-A1', 'FC3-A1', 'FCz-A1', 'FC4-A2',
-    'FT8-A2', 'C3-A1', 'C4-A2', 'T4-A2', 'TP7-A1', 'CP3-A1', 'CPz-A2', 'CP4-A2', 'TP8-A2', 'T5-A1', 'P3-A1', 'Pz-A2',
-    'P4-A2', 'T6-A2', 'O1-A1', 'Oz-A2', 'O2-A2',
+    'FT8-A2', 'C3-A1', 'Cz-A2', 'C4-A2', 'T4-A2', 'TP7-A1', 'CP3-A1', 'CPz-A2', 'CP4-A2', 'TP8-A2','T5-A1', 'P3-A1',
+    'Pz-A2', 'P4-A2', 'T6-A2', 'O1-A1', 'Oz-A2', 'O2-A2',
 ]
-healthy = [
-    "S2", "S3", "S9", "S10", "S12", "S13", "S14", "S15", "S19", "S20", "S24", "S25", "S30", "S32", "S38", "S39", "S42",
-    "S46", "S29", "S6", "S23", "S47", "S49",
-]
-depressed_active = ["S1", "S4", "S5", "S7", "S8", "S11", "S16", "S17", "S18", "S21", "S22", "S26", "S27"]
-depressed_sham = ["S31", "S33", "S35", "S36", "S37", "S40", "S41", "S43", "S44", "S45"]
-depressed = depressed_active + depressed_sham
+els_with_t3a1 = electrodes_arranged.copy()
+els_with_t3a1.insert(13, 'T3-A1')
+els_with_t3a2 = electrodes_arranged.copy()
+els_with_t3a2.insert(13, 'T3-A2')
 A1ref = ["F7", "FT7", "T3", "TP7", "T5", "Fp1", "F3", "FC3", "C3", "CP3", "P3", "O1", "Fpz", "Fz", "FCz"]
 A2ref = ["Cz", "CPz", "Pz", "Oz", "Fp2", "F4", "FC4", "C4", "CP4", "P4", "O2", "F8", "FT8", "T4", "TP8", "T6"]
 # fmt: on
 
 
 def get_files():
-    # data_path = "/Users/erlahrafnkelsdottir/Documents/DepEEG/Data/tDCS_EEG_data/"
-    data_path = "Data/tDCS_EEG_data/"
+    data_path = "/Users/erlahrafnkelsdottir/Documents/DepEEG/Data/tDCS_EEG_data/"
+    # data_path = "Data/tDCS_EEG_data/"
     subject_folders = os.listdir(data_path)
     txt_file_paths = []
 
@@ -60,15 +57,17 @@ def clean_data():
     for file in files:
         df = pd.read_csv(file, sep="\t", index_col=False)
 
-        # First, subtract A1 and A2 from corresponding columns if it hasn't been done
+        # First, subtract A1 and A2 references from corresponding columns if it hasn't been done
         if not ("-A1" or "-A2") in df.columns[0]:
             df = correct_refs(df)
 
-        # Then, remove all unnecessary columns
-        df = df[df.columns.intersection(electrodes_arranged)]
-
-        # Then, order all columns identically
-        df = df[electrodes_arranged]
+        # Then, remove all unnecessary columns and arrange correctly
+        if 'T3-A1' in df.columns:
+            df = df[df.columns.intersection(els_with_t3a1)]
+            df = df[els_with_t3a1]
+        else:
+            df = df[df.columns.intersection(els_with_t3a2)]
+            df = df[els_with_t3a2]
 
         # Then, save in new file with _clean in "Depressed_cleaned" or "Healthy_cleaned" folders
 
