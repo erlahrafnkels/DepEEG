2df03589eda272a28fc2cdc7ffa0f9df9b6068e6
erlahrafnkels <erla.hrafnkelsdottir@gmail.com>
2022-03-07 16:32:18 +0100

    Changes to filtering and plotting

--- Scripts/data_preprocessing.py
+++ Scripts/data_preprocessing.py
@@ -7,8 +7,7 @@ import numpy as np
 import pandas as pd
 from omegaconf import OmegaConf
 from scipy import signal
-
-# from scipy.stats import zscore
+from scipy.stats import zscore
 
 warnings.filterwarnings("ignore")
 
@@ -35,6 +34,7 @@ healthy = [
 depressed_active = ["S1", "S4", "S5", "S7", "S8", "S11", "S16", "S17", "S18", "S21", "S22", "S26", "S27"]
 depressed_sham = ["S31", "S33", "S35", "S36", "S37", "S40", "S41", "S43", "S44", "S45"]
 depressed = depressed_active + depressed_sham
+samp_freq = 500  # Sample frequency, given in data description (Hz)
 # fmt: on
 config = OmegaConf.load("config.yaml")
 
@@ -91,84 +91,160 @@ def clean_data():
     return
 
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
+def notch_filter(data, low_freq, high_freq):
+    notch_freq = 50  # Frequency to be removed from signal (Hz)
+    quality_factor = (high_freq - low_freq) / m.sqrt(low_freq * high_freq)
+    b_notch, a_notch = signal.iirnotch(notch_freq, quality_factor, samp_freq)
+    freq, h = signal.freqz(b_notch, a_notch, fs=samp_freq)
+    signal_notched = signal.filtfilt(b_notch, a_notch, data)
+    return freq, h, signal_notched
+
+
+def butter_bandpass_filter(data, low_freq, high_freq, order=5):
+    nyq = 0.5 * samp_freq
+    low = low_freq / nyq
+    high = high_freq / nyq
     b, a = signal.butter(order, [low, high], btype="band")
     y = signal.filtfilt(b, a, data)
     return y
 
 
-def filter():
+def filter_signal(record):
+    # Read in data
     data_path = "Data/tDCS_EEG_data/Data_cleaned/"
-    s1_eo1_data = pd.read_csv(
-        data_path + "S1-Pre-EO1_EEG_cleaned.txt", sep="\t", index_col=False
-    )
-    """
-    s1_ec1_data = pd.read_csv(
-        data_path + "S1-Pre-EC1_EEG_cleaned.txt", sep="\t", index_col=False
-    )
-    s2_eo1_data = pd.read_csv(
-        data_path + "S2-Pre_EO_EEG_cleaned.txt", sep="\t", index_col=False
-    )
-    s2_ec1_data = pd.read_csv(
-        data_path + "S2-Pre-_EC1_EEG_cleaned.txt", sep="\t", index_col=False
-    )
-    """
+    data = pd.read_csv(data_path + record, sep="\t", index_col=False)
+    datanp = data.to_numpy()
 
-    # Plot the raw signal
-    plt.figure("raw")
-    plt.plot(s1_eo1_data.iloc[:, 0], color=config.colors.dtu_red)
-    plt.title("Example of raw data: Eyes open pre-treatment")
-    plt.grid()
+    rows, cols = data.shape
+    print(rows)
+    print(cols)
+
+    datareshape = np.reshape(datanp, (1, -1))
+    print(datareshape.shape)
+    data2d = np.reshape(datareshape, (-1, cols))
+
+    low_freq = 0.5
+    high_freq = 50
+
+    freq, h, signal_notched = notch_filter(datareshape, low_freq, high_freq)
+    signal_filtered = butter_bandpass_filter(signal_notched, low_freq, high_freq)
+
+    filtered2d = np.reshape(signal_filtered, (-1, cols))
+
+    plt.figure(1)
+    plt.plot(data)
+    plt.title("Dataframe")
+
+    plt.figure(2)
+    plt.plot(datanp)
+    plt.title("Numpy array")
+
+    plt.figure(3)
+    plt.plot(data2d)
+    plt.title("Numpy array reshaped")
+
+    plt.figure(4)
+    plt.plot(filtered2d)
+    plt.title("Numpy array reshaped filtered")
+
+    plt.show()
+
+    return
+
+
+def filter_and_plot(record):
+    # Read in data
+    data_path = "Data/tDCS_EEG_data/Data_cleaned/"
+    data = pd.read_csv(data_path + record, sep="\t", index_col=False)
+    # raw = data.iloc[:, 0] # Here we're only looking at the records of Fp1
+
+    datanp = data.to_numpy()
+    rows, cols = data.shape
+    datareshape = np.reshape(datanp, (1, -1))
 
     # Upper and lower bounds for bandpass filter
     low_freq = 0.5
     high_freq = 50
 
-    # Create notch filter
-    samp_freq = 500  # Sample frequency, given in data description (Hz)
-    notch_freq = 50  # Frequency to be removed from signal (Hz)
-    quality_factor = (high_freq - low_freq) / m.sqrt(low_freq * high_freq)
-    b_notch, a_notch = signal.iirnotch(notch_freq, quality_factor, samp_freq)
-    # Outputs of signal.freqz below:
-    freq, h = signal.freqz(b_notch, a_notch, fs=samp_freq)
-
-    # Plot notch filter
-    # In the plot we transform h from complex numbers to dBs
-    plt.figure("notch filter")
-    plt.plot(freq, 20 * np.log10(abs(h)), color=config.colors.dtu_red)
-    plt.title("Notch filter")
-    plt.xlabel("Frequency [Hz]")
-    plt.ylabel("Amplitude [dB]")
-    plt.grid()
-
     # Filter out the 50 Hz powerline interference
-    signal_notched = signal.filtfilt(b_notch, a_notch, s1_eo1_data.iloc[:, 0])
+    freq, h, signal_notched = notch_filter(datareshape, low_freq, high_freq)
 
     # Apply bandpass filter
-    signal_filtered = butter_bandpass_filter(
-        signal_notched, low_freq, high_freq, samp_freq
-    )  # , order=6)
+    signal_filtered = butter_bandpass_filter(signal_notched, low_freq, high_freq)
 
-    # Plot the power spectral density of the raw, notched and filtered signals
-    plt.figure("psd")
-    plt.psd(s1_eo1_data.iloc[:, 0], Fs=samp_freq, color=config.colors.dtu_red)
-    plt.psd(signal_notched, Fs=samp_freq, color=config.colors.black)
-    plt.psd(signal_filtered, Fs=samp_freq, color=config.colors.blue)
-    plt.legend(labels=["Raw", "Notched", "Bandpass + notched"])
-    plt.title("Power spectral density of example")
+    signal_notched = np.reshape(signal_notched, (-1, cols))
+    signal_filtered = np.reshape(signal_filtered, (-1, cols))
 
-    # Plot the original signal with the filters
-    plt.figure("bandpass")
-    plt.plot(signal_filtered, color=config.colors.dtu_red)
-    plt.title("Example filtered")
-    plt.grid()
+    # Make one big plot
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
 
-    plt.show()
+    # Raw signal
+    axs[0, 0].plot(datanp[:, 0], color=config.colors.dtu_red)
+    axs[0, 0].set_title("Raw data")
+    axs[0, 0].grid()
 
-    return
+    # Notch filter
+    # In the plot we transform h from complex numbers to dBs
+    axs[0, 1].plot(freq, 20 * np.log10(abs(h)), color=config.colors.dtu_red)
+    axs[0, 1].set_title("Notch filter")
+    axs[0, 1].set_xlabel("Frequency [Hz]")
+    axs[0, 1].set_ylabel("Amplitude [dB]")
+    axs[0, 1].grid()
+
+    # Power spectral density of the raw, notched and filtered signals
+    axs[1, 0].psd(datanp[:, 0], Fs=samp_freq, color=config.colors.dtu_red)
+    axs[1, 0].psd(signal_notched[:, 0], Fs=samp_freq, color=config.colors.black)
+    axs[1, 0].psd(signal_filtered[:, 0], Fs=samp_freq, color=config.colors.blue)
+    axs[1, 0].legend(labels=["Raw", "Notched", "Bandpass + notched"])
+    axs[1, 0].set_title("Power spectral density")
+
+    # Original signal with the filters
+    axs[1, 1].plot(signal_filtered[:, 0], color=config.colors.dtu_red)
+    axs[1, 1].set_title("Data filtered")
+    axs[1, 1].grid()
+
+    fig.suptitle(record[:-4])
+    fig.tight_layout()
+
+    # Now, normalize data with mean = 0 and std = 1
+    # Find one mean and std for all channels and the whole 2 minute recording
+    print("SIGNAL RAW SHAPE:", datanp.shape)
+    print("SIGNAL RAW MEAN:", datanp.mean())
+    print("SIGNAL RAW STD:", datanp.std())
+
+    print("SIGNAL FILTERED SHAPE:", signal_filtered.shape)
+    print("SIGNAL FILTERED MEAN:", signal_filtered.mean())
+    print("SIGNAL FILTERED STD:", signal_filtered.std())
+
+    data_norm = zscore(signal_filtered, axis=None)
+    print("DATA NORM SHAPE:", data_norm.shape)
+    print("DATA NORM MEAN:", data_norm.mean())
+    print("DATA NORM STD:", data_norm.std())
+    print()
+    print("Normalized, mean of first row:", data_norm[0, :].mean())
+    print()
+    print()
+
+    mean = data.stack().mean()
+    std = data.stack().std()
+    data_norm2 = (data - mean) / std
+
+    print("DATA NORM 2 MEAN:", data_norm2.mean())
+    print("DATA NORM 2 STD:", data_norm2.std())
+    print()
+    print("Normalized 2, mean of first row:", data_norm2[0, :].mean())
+
+    # Convert recordings to time in seconds
+    # time_axis = range(0, m.floor(data.shape[0] / samp_freq))
+
+    # Plot all channels
+    fig2, axs2 = plt.subplots(nrows=31, ncols=1, sharex=True, figsize=(12, 8))
+    for i in range(0, 31):
+        axs2[i].plot(signal_filtered[:, i])
+        # axs2[i].set_xticks(time_axis)
+        axs2[i].set_frame_on(False)
+
+    return fig, fig2
 
 
 if __name__ == "__main__":
@@ -179,4 +255,10 @@ if __name__ == "__main__":
     else:
         print("Cleaned files in folder.")
 
-    filter()
+    fig1, fig2 = filter_and_plot("S1-Pre-EC1_EEG_cleaned.txt")
+    # fig2 = filter_and_plot("S1-post_EC1_EEG_cleaned.txt")
+    # fig3 = filter_and_plot("S42_pre_EC1_cleaned.txt")
+    # fig4 = filter_and_plot("S42_EC1Post_EEG_cleaned.txt")
+    plt.show()
+
+    # filter_signal("S1-Pre-EC1_EEG_cleaned.txt")
